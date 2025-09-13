1145f3ec9e4998a3a329e24ef74c421f8502f796
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-09-13 22:45:06 +0530

    portfolio websit creating

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './section/Navbar'
+import Hero from './section/Hero'
+
+function App() {
+  return (
+    <div className=' container mx-auto max-w-7xl'>
+      <Navbar/>
+      <Hero/>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+
+    </div>
+  )
+}
+
+export default App
